bf0b0c9fd4493c7f1ba301e3f91ec120bf3ab8e5
chuotduoicho <83025674+chuotduoicho@users.noreply.github.com>
2022-04-16 09:08:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
+const certificateRoute = require("./routes/certificates");
 // const multer = require("multer");
 // const path = require("path");
 const PORT = process.env.PORT || 5000;
@@ -46,6 +47,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
+app.use("/api/certificates", certificateRoute);
 
 app.listen(PORT, () => {
   console.log("Backend is running.");
